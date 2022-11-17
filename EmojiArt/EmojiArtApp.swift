0f7c6a2b98f1686bb4ad69a1c477c432cc7dfe4c
晒晕的兔子 <gubu1986@126.com>
2022-11-17 12:22:03 +0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by 顾艳华 on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
