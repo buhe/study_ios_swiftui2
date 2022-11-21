5892eed70e7e271469455d2a31ad39e1243486ee
晒晕的兔子 <gubu1986@126.com>
2022-11-21 18:19:23 +0800

    1 model

--- EmojiArt/Model.swift
+++ EmojiArt/Model.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct Model {
+    var background: Background = .blank
+    var emojis = [Emoji]()
+    
+    enum Background {
+        case blank
+        case url(URL)
+        case imageData(Data)
+        
+        func url() -> URL? {
+            switch self {
+            case .url(let url): return url
+            default: return nil
+            }
+        }
+        
+        func data() -> Data? {
+            switch self {
+            case .imageData(let data): return data
+            default: return nil
+            }
+        }
+    }
+    
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int
+        var y: Int
+        var size: Int
+        let id: Int
+
+    }
+    var idGen = 0
+    mutating func add(_ text: String, at location: (x: Int, y: Int), _ size: Int) {
+        idGen += 1
+        emojis.append(Emoji(text: text, x: location.x, y: location.y, size: size, id: idGen))
+    }
+}
