ac381a69fcfcba3d21dafcffe0d3f3b1c2cb2d37
晒晕的兔子 <gubu1986@126.com>
2022-11-21 19:02:25 +0800

    1 vm

--- EmojiArt/ViewModel.swift
+++ EmojiArt/ViewModel.swift
@@ -5,4 +5,25 @@
 //  Created by 顾艳华 on 2022/11/21.
 //
 
-import Foundation
+import SwiftUI
+
+class ViewModel: ObservableObject {
+    @Published var model: Model
+    
+    init() {
+        model = Model()
+    }
+    
+    func move(_ e: Model.Emoji,to offset: CGSize) {
+        if let i = model.emojis.index(matching: e) {
+            model.emojis[i].x += Int(offset.width)
+            model.emojis[i].y += Int(offset.height)
+        }
+    }
+    
+    func scale(_ e: Model.Emoji, by scale: CGFloat) {
+        if let i = model.emojis.index(matching: e) {
+            model.emojis[i].size = Int((CGFloat(model.emojis[i].size) * scale).rounded(.toNearestOrAwayFromZero))
+        }
+    }
+}
