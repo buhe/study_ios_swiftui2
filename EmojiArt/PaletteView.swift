02c129e88ec5ed65bd5c2e09c2d2d93b5f9e7fd2
晒晕的兔子 <gubu1986@126.com>
2022-11-25 18:06:25 +0800

    12  part1

---
+++ EmojiArt/PaletteView.swift
@@ -0,0 +1,20 @@
+//
+//  PaletteView.swift
+//  EmojiArt
+//
+//  Created by 顾艳华 on 2022/11/25.
+//
+
+import SwiftUI
+
+struct PaletteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaletteView_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteView()
+    }
+}
